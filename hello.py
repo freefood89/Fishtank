85da9580399f44f6cf066ba0b821b4cc0aa9ad1d
coryz <coryz@umich.edu>
2014-09-30 00:54:41 -0400

    The Initial night of Programming. Shitty way of toggling Fish LEDControl Dictionary Variable

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
+LEDControl = {};
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -8,7 +10,7 @@ def hello_world():
 @app.route('/login', methods=['POST','GET'])
 def login():
     error = None
-    if request.method == 'POST':    
+    if request.method == 'POST':
         if valid_login(request.form['username'], request.form['password']):
             return log_user_in(request.form['username'])
         else:
@@ -21,6 +23,20 @@ def log_user_in(name):
 def valid_login(username, password):
     print((username, password))
     return True
+
+
+@app.route('/feed', methods=['POST','GET'])
+def feed():
+    LEDControl['LED State'] = 'On'
+
+@app.route('/toggle', methods=['POST'])
+def toggle():
+    LEDControl['LED State'] = 'Off'
+    return 'hello_world'
+
+@app.route('/state', methods=['POST','GET'])
+def state():
+    return LEDControl['LED State']
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
