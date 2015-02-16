9f173907fa7fa9abfb2983bc36e795a54f38f579
freefood89 <freefood89@gmail.com>
2015-02-16 01:53:54 -0500

    onoff switches fixed and legacy onoff switch js removed

--- templates/static/js/dashboard.js
+++ templates/static/js/dashboard.js
@@ -2,26 +2,14 @@ $(document).ready(function(){
     var alertbox = $('#check');
     var sensorbox = $('#sensor');
     var imagebox = $('#image');
-    var toggleButon = $('#toggle').button();
+    // var toggleButon = $('#toggle').button();
     var deviceOptions = $('#device_id');
     var onoffDiv = jQuery('<div/>',{class:'onoffswitch', text:'Hello'}).html();
     var inputType = jQuery('<input>',{type:'checkbox',name:'onoffswitch',class:'onoffswitch-checkbox',id:'image'});
 
     //<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="image">
 
-    $.getJSON("/devices",function(data){
-        $.each(data, function(ind) {
-            console.log(data)
-            deviceOptions.append($("<option></option>").attr("value",data[ind]).text(data[ind]));
-            $("#switches").append('<div class="switchbox">'+data[ind] + '<div class="onoffswitch"> <input type="checkbox" onchange=deviceToggle(' + this + ') name="onoffswitch" class="onoffswitch-checkbox" id= "' + data[ind] + '"><label class="onoffswitch-label" for="' + data[ind]+ '"><span class="onoffswitch-inner"></span> <span class="onoffswitch-switch"></span> </div></div>');
-
-        });
-    });
-
     setInterval(function(){
-        if (alertbox[0].checked){
-            alert('hello');
-        }
         if (sensorbox[0].checked){
             $.get("/sensors/oxygen?t="+now.toString(), function(data){
                 //$("p").text(JSON.stringify(data.oxygen));
@@ -37,18 +25,6 @@ $(document).ready(function(){
     }, 3000);
 });
 
-var now = (new Date()).getTime();
-var data = [[[now,1],[now+100000,2],[now+200000,3],[now+300000,2]]];
-var options = {
-    xaxis: {
-        mode: "time",
-        timeformat: "%m/%d %H:%M"
-    },
-    series: {
-        lines: {show: true}
-    }
-};
-$.plot($("#placeholder"),data,options);
 
 function deviceToggle(deviceIndex) {
 
