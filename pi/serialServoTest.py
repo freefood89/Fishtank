446aec0e54102f965b0d08ed1042b2a4565fb809
freefood89 <freefood89@gmail.com>
2015-02-24 21:48:40 -0500

    changed serial port for linux machine

--- pi/serialServoTest.py
+++ pi/serialServoTest.py
@@ -1,6 +1,7 @@
 import serial
 
-ser = serial.Serial('/dev/tty.usbmodem1441', 9600)
+#ser = serial.Serial('/dev/tty.usbmodem1441', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 if not ser.isOpen():
 	print('opening serial port')
 	ser.open()
