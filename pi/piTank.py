a2f8893dcc6c5e1d6203d03cd8e98499718decba
freefood89 <freefood89@gmail.com>
2015-03-03 21:57:00 -0500

    pitank communicates with arduino

--- pi/piTank.py
+++ pi/piTank.py
@@ -1,53 +1,59 @@
 import picamera
-import RPIO
 import time
 import requests
 import json
 import signal
 import sys
+import serial
 
-LEDMap = {'led1':17}
-RPIOMap = {'On': True, 'Off': False}
+DeviceMap = {'led1':'led_1', 'led2':'led_2'}
+OutputMap = {'On': 200, 'Off': 0, 'left'}
 
 def exit_gracefully(signum, frame):
     signal.signal(signal.SIGINT, original_sigint)
     try:
         if input('¥nReally quit? (y/n)>').lower().startswith('y'):
-            print('Cleaning Up RPIO')
-            RPIO.cleanup()
+            # print('Cleaning Up RPIO')
+            # RPIO.cleanup()
             sys.exit(1)
     except KeyboardInterrupt:
         print('quitting')
         sys.exit(1)
 
-def update_LEDs(state):
-    for key in state:
-        if key not in LEDMap or state[key] not in RPIOMap:
+def update_Devices(state):
+    for led, value in state:
+        if led not in LEDMap or state[key] not in OutputMap:
             #print('could not map')
             break
-        #print(key,state[key])
-        #print(LEDMap[key],RPIOMap[state[key]])
-        RPIO.output(LEDMap[key], RPIOMap[state[key]])
+        serialOut = ' '.join([LEDMap[led], str(OutputMap[value])])
+        ser.write((serialOut+'\n').encode())
+        print(ser.readline().strip().decode('utf-8'))
 
-def init_LEDs():
-    for led in LEDMap:
-        RPIO.setup(LEDMap[led], RPIO.OUT, initial=RPIO.LOW)
+def log(message):
+    output = message #add time stamp
+    print(output)
+    #add write to logger
+
+# def init_LEDs():
+#     for led in LEDMap:
+#         RPIO.setup(LEDMap[led], RPIO.OUT, initial=RPIO.LOW)
 
 if __name__ == '__main__':
     original_sigint = signal.getsignal(signal.SIGINT)
     signal.signal(signal.SIGINT, exit_gracefully)
+    ser = serial.Serial('/dev/ttyACM0',9600)
 
-    init_LEDs()
+    # init_LEDs()
     while True:
         t1 = time.time()
         try:
             r = requests.get('http://renomania.ddns.net/~/state')
-            leds = json.loads(r.text)
+            devices = json.loads(r.text)
         except Exception as e:
             print(e)
         else:
             #print('Updating LEDs')
-            update_LEDs(leds)
+            update_Devices(devices)
         with picamera.PiCamera() as camera:
             camera.resolution=(640,480)
             camera.capture('image.jpg')
