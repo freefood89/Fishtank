09f79135b4902886a6328174fe7ff6c8e81fa348
freefood89 <freefood89@gmail.com>
2015-05-24 22:47:36 -0400

    restructured files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from hello import app
+from fishtankui.fishtankui import app
 
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(8081,'127.0.0.1')
